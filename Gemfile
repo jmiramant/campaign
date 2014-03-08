ce091f0ea724efb26b059104eaf2aabf4578b5c3
Josh Miramant <josh@miramant.me>
2014-03-08 07:05:25 -0800

    updated pg db

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+gem 'pg'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'bourbon'
+  gem 'neat'
+  gem 'haml'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'capistrano'
+gem 'capistrano-rails'
+
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campaign::Application.config.secret_token = 'b6224698e9bb8aaf2b5c087b207f7dc52dd2438d8219310718d5450dbfd19ef13435b9df34479d6b998a49a3e36905a14f0c336fbb19632973ed912e3e597659'
